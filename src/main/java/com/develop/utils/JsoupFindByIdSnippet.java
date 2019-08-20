6071d11dd586957140bae772c6013ff194637ac2
Yegor Bobyk <ybobuk@tibco.com>
2019-08-20 17:35:45 +0300

    Add utils files and dependencies

---
+++ src/main/java/com/develop/utils/JsoupFindByIdSnippet.java
@@ -0,0 +1,50 @@
+package com.develop.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JsoupFindByIdSnippet {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(JsoupFindByIdSnippet.class);
+
+    private static String CHARSET_NAME = "utf8";
+
+    public static void main(String[] args) {
+        String resourcePath = "./samples/startbootstrap-freelancer-gh-pages-cut.html";
+        String targetElementId = "sendMessageButton";
+
+        Optional<Element> buttonOpt = findElementById(new File(resourcePath), targetElementId);
+
+        Optional<String> stringifiedAttributesOpt = buttonOpt.map(button ->
+                button.attributes().asList().stream()
+                        .map(attr -> attr.getKey() + " = " + attr.getValue())
+                        .collect(Collectors.joining(", "))
+        );
+
+        stringifiedAttributesOpt.ifPresent(attrs -> LOGGER.info("Target element attrs: [{}]", attrs));
+    }
+
+    private static Optional<Element> findElementById(File htmlFile, String targetElementId) {
+        try {
+            Document doc = Jsoup.parse(
+                    htmlFile,
+                    CHARSET_NAME,
+                    htmlFile.getAbsolutePath());
+
+            return Optional.of(doc.getElementById(targetElementId));
+
+        } catch (IOException e) {
+            LOGGER.error("Error reading [{}] file", htmlFile.getAbsolutePath(), e);
+            return Optional.empty();
+        }
+    }
+
+}
